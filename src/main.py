9dc3126f9cd7f429c7d915fcfb8b86e65eaf7562
Omen <occultnerg@gmail.com>
2024-02-18 13:56:37 +0200

    nested menu navigation complete.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from  src.ContactManager.contact_manager import run_contact_manager
 from src.NoteManger.note_manager import run_note_manager
 from src.EventManager.event_manager import run_event_manager
 from src.View.base_view import ConsoleView
@@ -9,15 +10,18 @@ def main():
     while True:
         program_name = "Main Menu"
         options = {
-            '1': 'Note Manager',
-            '2': 'Event Manager',
+            '1': 'Contact Manager',
+            '2': 'Note Manager',
+            '3': 'Event Manager',
             '0': 'Exit'
         }
         choice = view.display_menu(program_name, options)
 
         if choice == '1':
-            run_note_manager()
+            run_contact_manager()
         elif choice == '2':
+            run_note_manager()
+        elif choice == '3':
             run_event_manager()
         elif choice == '0':
             exit()

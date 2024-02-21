176b43a940804b147a1eeac0ca118b206a15bbae
AkaVelial13 <155759473+AkaVelial13@users.noreply.github.com>
2024-02-21 20:30:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
       name='task-manager',
       version='1.0',
       py_modules= ['bot', 'models', 'contact_manager', 'event_manager', 'note_manager', 'file_sorter', 'base_view', 'common'],
-      description='It`s a personal helper, witch can be use like the address book, notes manager and file sorter.',
+      description='It`s a personal helper, witch can be use like the address book, notes manager, event manager and file sorter.',
       url='',
-      author='',
-      author_email='occultnerg@gmail.com',
+      author='Dreamcode team, Vitaliy Nerg, Omelchenko Anton, Artem Hrytsay, Serhii Nozhenko, Muzychyk Vadym',
+      author_email='occultnerg2@gmail.com, omelchenko230783@gmail.com, artem_madrid@hotmail.com, neprokaren41@gmail.com',
       license='MIT',
       install_requires=[
             'tabulate==0.9.0',
